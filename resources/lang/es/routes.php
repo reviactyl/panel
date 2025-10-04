5395be173a69688af1ea73a8f0bf600d30d735e8
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-04 10:00:17 +0530

    New translations routes.php (Spanish)

---
+++ resources/lang/es/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'account' => [
+        'overview' => 'Account Overview',
+        'api' => 'API Keys',
+        'ssh' => 'SSH Keys',
+        'activity' => 'Activity',
+    ],
+    'server' => [
+        'console' => 'Console',
+        'files' => 'Files',
+        'startup' => 'Startup',
+        'network' => 'Network',
+        'databases' => 'Databases',
+        'schedules' => 'Schedules',
+        'backups' => 'Backups',
+        'users' => 'Users',
+        'settings' => 'Settings',
+        'activity' => 'Activity',
+    ],
+];
