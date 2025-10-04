97c6a972fba8965f54dbb29853941e611a7c881c
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-04 10:00:29 +0530

    New translations index.php (Spanish)

---
+++ resources/lang/es/dashboard/index.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title' => 'Dashboard',
+    'other-servers' => 'Showing others\' servers',
+    'your-servers' => 'Showing your servers',
+    'no-servers' => 'There are no servers associated with your account.',
+    'no-other-servers' => 'There are no other servers to display.',
+    'server' => [
+        'online' => 'Online',
+        'offline' => 'Offline',
+        'starting' => 'Starting',
+        'stopping' => 'Stopping',
+        'suspended' => 'Suspended',
+        'connection-error' => 'Connection Error',
+        'transferring' => 'Transferring',
+        'installing' => 'Installing',
+        'unavailable' => 'Unavailable',
+    ],
+];
