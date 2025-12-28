c6370ac901406038351cc2947013944c358687f1
Reviactyl CI <github-actions[bot]@users.noreply.github.com>
2025-12-28 10:03:53 +0000

    ci(release): bump version to 2.1.2 and build to 02010200

--- config/app.php
+++ config/app.php
@@ -11,8 +11,8 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => 'canary',
-    'build' => '00000000',
+    'version' => '2.1.2',
+    'build' => '02010200',
 
     /*
     |--------------------------------------------------------------------------
