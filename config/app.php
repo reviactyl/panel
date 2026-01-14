d9e69dbf6f3c58643525511fd6a3a7d657726c97
Reviactyl CI <ci@reviactyl.dev>
2026-01-14 08:59:57 +0000

    ci(release): bump version

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => 'canary',
+    'version' => '2.2.0',
 
     /*
     |--------------------------------------------------------------------------
