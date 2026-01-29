3b68073533f24b29e4e1e78d737b25d7e0a0e1a5
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-01-29 16:05:06 +0530

    fix addons and linting

--- resources/scripts/components/elements/Code.tsx
+++ resources/scripts/components/elements/Code.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 interface CodeProps {
-    children: React.ReactChild | React.ReactFragment | React.ReactPortal;
+    dark?: boolean;
+    children: React.ReactNode;
 }
 
-export default ({ children }: CodeProps) => (
-    <code className={'font-mono text-sm px-2 py-1 inline-block rounded-ui bg-gray-800 border border-gray-600'}>
-        {children}
-    </code>
-);
+export default ({ dark, children }: CodeProps) => {
+    void dark; // legacy prop, we actually dont need it. 
+
+    return (
+        <code className="font-mono text-sm px-2 py-1 inline-block rounded-ui bg-gray-800 border border-gray-600">
+            {children}
+        </code>
+    );
+};
--- resources/scripts/components/elements/Field.tsx
+++ resources/scripts/components/elements/Field.tsx
@@ -6,12 +6,14 @@ import tw from 'twin.macro';
 import styled from 'styled-components/macro';
 
 interface OwnProps {
+    light?: boolean;
     name: string;
     icon?: React.ComponentType<{ className?: string }>;
     label?: string;
     description?: string;
     validate?: (value: any) => undefined | string | Promise<any>;
 }
+
 const IconWrapper = styled.div`
     ${tw`bg-gray-600 border-2 !border-r-0 rounded-l-ui p-3`}
 `;
@@ -19,46 +21,54 @@ const IconWrapper = styled.div`
 type Props = OwnProps & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'name'>;
 
 const Field = forwardRef<HTMLInputElement, Props>(
-    ({ id, name, icon: Icon, label, description, validate, ...props }, ref) => (
-        <FormikField innerRef={ref} name={name} validate={validate}>
-            {({ field, form: { errors, touched } }: FieldProps) => (
-                <div>
-                    {label && <Label htmlFor={id}>{label}</Label>}
-                    <div className='flex items-center'>
-                        {Icon && (
-                            <IconWrapper
-                                className={
-                                    touched[field.name] && errors[field.name]
-                                        ? 'border-red-400 text-red-400'
-                                        : 'border-gray-500 text-gray-500'
-                                }
-                            >
-                                <Icon className='w-5 h-5' />
-                            </IconWrapper>
-                        )}
-                        <Input
-                            css={[Icon && tw`!rounded-l-none`]}
-                            id={id}
-                            {...field}
-                            {...props}
-                            hasError={!!(touched[field.name] && errors[field.name])}
-                        />
-                    </div>
+    ({ id, name, light = false, icon: Icon, label, description, validate, ...props }, ref) => {
+        void light; // legacy prop, we actually dont need it. 
+
+        return (
+            <FormikField innerRef={ref} name={name} validate={validate}>
+                {({ field, form: { errors, touched } }: FieldProps) => (
                     <div>
-                        {touched[field.name] && errors[field.name] ? (
-                            <p className={'mt-1 text-xs text-red-200'}>
-                                {(errors[field.name] as string).charAt(0).toUpperCase() +
-                                    (errors[field.name] as string).slice(1)}
-                            </p>
-                        ) : description ? (
-                            <p className={'mt-1 text-xs text-gray-200'}>{description}</p>
-                        ) : null}
+                        {label && <Label htmlFor={id}>{label}</Label>}
+
+                        <div className="flex items-center">
+                            {Icon && (
+                                <IconWrapper
+                                    className={
+                                        touched[field.name] && errors[field.name]
+                                            ? 'border-red-400 text-red-400'
+                                            : 'border-gray-500 text-gray-500'
+                                    }
+                                >
+                                    <Icon className="w-5 h-5" />
+                                </IconWrapper>
+                            )}
+
+                            <Input
+                                css={[Icon && tw`!rounded-l-none`]}
+                                id={id}
+                                {...field}
+                                {...props}
+                                hasError={!!(touched[field.name] && errors[field.name])}
+                            />
+                        </div>
+
+                        <div>
+                            {touched[field.name] && errors[field.name] ? (
+                                <p className="mt-1 text-xs text-red-200">
+                                    {(errors[field.name] as string).charAt(0).toUpperCase() +
+                                        (errors[field.name] as string).slice(1)}
+                                </p>
+                            ) : description ? (
+                                <p className="mt-1 text-xs text-gray-200">{description}</p>
+                            ) : null}
+                        </div>
                     </div>
-                </div>
-            )}
-        </FormikField>
-    )
+                )}
+            </FormikField>
+        );
+    }
 );
+
 Field.displayName = 'Field';
 
 export default Field;
