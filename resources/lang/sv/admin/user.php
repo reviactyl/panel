e56ed0c703a936b99d50b2a0d9d72d47a96e008e
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-12 00:34:09 +0530

    New translations user.php (Swedish)

--- resources/lang/sv/admin/user.php
+++ resources/lang/sv/admin/user.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'User',
+    'title' => 'Användare',
     'exceptions' => [
         'delete_self' => 'Du kan inte ta bort ditt eget konto.',
         'user_has_servers' => 'Kan inte ta bort en användare med aktiva servrar kopplade till deras konto. Vänligen ta bort deras servrar innan du fortsätter.',
@@ -11,18 +11,18 @@ return [
         'account_updated' => 'Kontot har uppdaterats framgångsrikt.',
     ],
     'details' => [
-        'account_details' => 'Account Details',
-        'external_id' => 'External ID',
-        'username' => 'Username',
-        'email' => 'Email Address',
-        'first_name' => 'First Name',
-        'last_name' => 'Last Name',
-        'language' => 'Language',
-        'password' => 'Password',
-        'password_confirmation' => 'Confirm Password',
-        'root_admin' => 'Root Administrator',
-        'root_admin_desc' => 'This user will have full access to all servers and settings on the system.',
-        'privileges' => 'Privileges',
-        'admin_status' => 'Admin Status',
+        'account_details' => 'Kontouppgifter',
+        'external_id' => 'Externt ID',
+        'username' => 'Användarnamn',
+        'email' => 'E-postadress',
+        'first_name' => 'Förnamn',
+        'last_name' => 'Efternamn',
+        'language' => 'Språk',
+        'password' => 'Lösenord',
+        'password_confirmation' => 'Bekräfta lösenord',
+        'root_admin' => 'Root-administratör',
+        'root_admin_desc' => 'Denna användare kommer att ha full åtkomst till alla servrar och inställningar i systemet.',
+        'privileges' => 'Behörigheter',
+        'admin_status' => 'Administratörsstatus',
     ],
 ];
