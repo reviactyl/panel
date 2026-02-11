1abfbf7b14f20c14f30532fcc5cb89a67b280eab
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-12 00:34:11 +0530

    New translations auth.php (Swedish)

--- resources/lang/sv/auth.php
+++ resources/lang/sv/auth.php
@@ -18,7 +18,7 @@ return [
         'google' => 'Google',
         'discord' => 'Discord',
         'github' => 'GitHub',
-        'not_linked' => 'This account has not been linked to any :provider account. Please log in with your email and password first, then link your :provider account in the Account Settings page.',
+        'not_linked' => 'Detta konto har inte kopplats till något :provider-konto. Logga in med din e-postadress och ditt lösenord först och koppla sedan ditt :provider-konto på sidan Kontoinställningar.',
     ],
 
     'forgot-password' => [
@@ -53,15 +53,15 @@ return [
     ],
 
     'register' => [
-        'no-match' => 'Your password does not match.',
-        'namefirst-label' => 'First Name',
-        'namelast-label' => 'Last Name',
-        'email-label' => 'Email',
-        'username-label' => 'UserName',
-        'password-label' => 'Password',
-        'min-length' => 'Passwords must be at least 8 characters in length.',
-        'confirm-label' => 'Confirm Password',
-        'label' => 'Register',
+        'no-match' => 'Ditt lösenord stämmer inte överens.',
+        'namefirst-label' => 'Förnamn',
+        'namelast-label' => 'Efternamn',
+        'email-label' => 'E-post',
+        'username-label' => 'Användarnamn',
+        'password-label' => 'Lösenord',
+        'min-length' => 'Lösenord måste vara minst 8 tecken långt.',
+        'confirm-label' => 'Bekräfta lösenord',
+        'label' => 'Registrera',
     ],
     
     'failed' => 'Ingen matchande konto med dessa uppgifter kunde hittas.',
