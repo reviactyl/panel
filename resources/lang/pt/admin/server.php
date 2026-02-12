0503cb0d047309022e95f4bd17adaed991a93d74
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-12 20:44:30 +0530

    New translations server.php (Portuguese, Brazilian)

--- resources/lang/pt/admin/server.php
+++ resources/lang/pt/admin/server.php
@@ -183,14 +183,6 @@ return [
         'view' => 'Ver',
     ],
 
-    'alerts' => [
-        'install_toggled' => 'O status de instalação do servidor foi alterado.',
-        'server_suspended' => 'O servidor foi :action.',
-        'server_reinstalled' => 'A reinstalação do servidor foi iniciada.',
-        'server_deleted' => 'O servidor foi excluído.',
-        'server_delete_failed' => 'Falha ao excluir o servidor.',
-    ],
-    
     'exceptions' => [
         'no_new_default_allocation' => 'Você está tentando excluir a alocação padrão para este servidor, mas não há alocação de fallback para usar.',
         'marked_as_failed' => 'Este servidor foi marcado como tendo falhado em uma instalação anterior. O status atual não pode ser alternado neste estado.',
@@ -198,15 +190,18 @@ return [
         'daemon_exception' => 'Houve uma exceção ao tentar se comunicar com o daemon resultando em um código de resposta HTTP/:code. Esta exceção foi registrada. (id da requisição: :request_id)',
         'default_allocation_not_found' => 'A alocação padrão solicitada não foi encontrada nas alocações deste servidor.',
     ],
+
     'alerts' => [
+        'install_toggled' => 'O status de instalação do servidor foi alterado.',
+        'server_suspended' => 'O servidor foi :action.',
+        'server_reinstalled' => 'A reinstalação do servidor foi iniciada.',
+        'server_deleted' => 'Server has been deleted from the system.',
+        'server_delete_failed' => 'Falha ao excluir o servidor.',
         'startup_changed' => 'A configuração de inicialização para este servidor foi atualizada. Se o ninho ou egg deste servidor foi alterado, uma reinstalação ocorrerá agora.',
-        'server_deleted' => 'O servidor foi excluído com sucesso do sistema.',
         'server_created' => 'O servidor foi criado com sucesso no painel. Por favor, aguarde alguns minutos para que o daemon instale completamente este servidor.',
         'build_updated' => 'Os detalhes de build para este servidor foram atualizados. Algumas alterações podem requerer uma reinicialização para entrar em vigor.',
         'suspension_toggled' => 'O status de suspensão do servidor foi alterado para :status.',
         'rebuild_on_boot' => 'Este servidor foi marcado como requerendo uma reconstrução do Container Docker. Isso acontecerá na próxima vez que o servidor for iniciado.',
-        'install_toggled' => 'O status de instalação para este servidor foi alternado.',
-        'server_reinstalled' => 'Este servidor foi colocado na fila para uma reinstalação começando agora.',
         'details_updated' => 'Os detalhes do servidor foram atualizados com sucesso.',
         'docker_image_updated' => 'A imagem Docker padrão para usar neste servidor foi alterada com sucesso. Uma reinicialização é necessária para aplicar esta alteração.',
         'node_required' => 'Você deve ter pelo menos um nó configurado antes de poder adicionar um servidor a este painel.',
