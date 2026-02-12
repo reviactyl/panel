9309eac07041e33d4fc8954fce81979d31fdd4c9
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-12 20:44:22 +0530

    New translations server.php (German)

--- resources/lang/de/admin/server.php
+++ resources/lang/de/admin/server.php
@@ -183,14 +183,6 @@ return [
         'view' => 'Ansehen',
     ],
 
-    'alerts' => [
-        'install_toggled' => 'Server-Installationsstatus wurde umgeschaltet.',
-        'server_suspended' => 'Server wurde :action.',
-        'server_reinstalled' => 'Server-Neuinstallation wurde gestartet.',
-        'server_deleted' => 'Server wurde gelöscht.',
-        'server_delete_failed' => 'Server konnte nicht gelöscht werden.',
-    ],
-    
     'exceptions' => [
         'no_new_default_allocation' => 'Sie versuchen, die Standardzuweisung für diesen Server zu löschen, aber es gibt keine Fallback-Zuweisung.',
         'marked_as_failed' => 'Dieser Server wurde als fehlgeschlagen bei einer vorherigen Installation markiert. Der aktuelle Status kann in diesem Zustand nicht umgeschaltet werden.',
@@ -198,15 +190,18 @@ return [
         'daemon_exception' => 'Es trat ein Fehler bei der Kommunikation mit dem Daemon auf, der zu einem HTTP/:code Antwortcode führte. Dieser Fehler wurde protokolliert. (Anfrage-ID: :request_id)',
         'default_allocation_not_found' => 'Die angeforderte Standardzuweisung wurde in den Zuweisungen dieses Servers nicht gefunden.',
     ],
+
     'alerts' => [
+        'install_toggled' => 'Server-Installationsstatus wurde umgeschaltet.',
+        'server_suspended' => 'Server wurde :action.',
+        'server_reinstalled' => 'Server-Neuinstallation wurde gestartet.',
+        'server_deleted' => 'Server has been deleted from the system.',
+        'server_delete_failed' => 'Server konnte nicht gelöscht werden.',
         'startup_changed' => 'Die Startkonfiguration für diesen Server wurde aktualisiert. Wenn das Nest oder Ei dieses Servers geändert wurde, findet jetzt eine Neuinstallation statt.',
-        'server_deleted' => 'Der Server wurde erfolgreich aus dem System gelöscht.',
         'server_created' => 'Der Server wurde erfolgreich auf dem Panel erstellt. Bitte geben Sie dem Daemon einige Minuten Zeit, um diesen Server vollständig zu installieren.',
         'build_updated' => 'Die Build-Details für diesen Server wurden aktualisiert. Einige Änderungen erfordern möglicherweise einen Neustart, um wirksam zu werden.',
         'suspension_toggled' => 'Der Suspendierungsstatus des Servers wurde auf :status geändert.',
         'rebuild_on_boot' => 'Dieser Server wurde so markiert, dass er einen Docker-Container-Rebuild erfordert. Dies geschieht beim nächsten Start des Servers.',
-        'install_toggled' => 'Der Installationsstatus für diesen Server wurde umgeschaltet.',
-        'server_reinstalled' => 'Dieser Server wurde für eine Neuinstallation in die Warteschlange gestellt, die jetzt beginnt.',
         'details_updated' => 'Serverdetails wurden erfolgreich aktualisiert.',
         'docker_image_updated' => 'Das Standard-Docker-Image für diesen Server wurde erfolgreich geändert. Ein Neustart ist erforderlich, um diese Änderung zu übernehmen.',
         'node_required' => 'Sie müssen mindestens einen Node konfiguriert haben, bevor Sie diesem Panel einen Server hinzufügen können.',
