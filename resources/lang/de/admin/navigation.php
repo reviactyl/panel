531be2af23f9a7f1a015af7caa58c7a4630a9fde
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-12 20:44:20 +0530

    New translations navigation.php (German)

--- resources/lang/de/admin/navigation.php
+++ resources/lang/de/admin/navigation.php
@@ -7,6 +7,7 @@ return [
         'dashboard' => 'Dashboard',
         'settings' => 'Einstellungen',
         'api' => 'Anwendungs-API',
+        'activity_log' => 'Activity Log',
     ],
     'management' => [
         'title' => 'Verwaltung',
