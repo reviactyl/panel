a092154d98b5d39daad284f81852c358b70a11ac
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-14 07:45:17 +0530

    New translations navigation.php (Swedish)

--- resources/lang/sv/admin/navigation.php
+++ resources/lang/sv/admin/navigation.php
@@ -8,6 +8,8 @@ return [
         'settings' => 'Inställningar',
         'api' => 'Applikations-API',
         'activity_log' => 'Activity Log',
+        'user_activity_metrics' => 'User Activity Metrics',
+        'no_data' => 'No data available.',
     ],
     'management' => [
         'title' => 'Hantering',
