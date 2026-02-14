8cbf7a72b8845a551ee0a18d879d02285325492c
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2026-02-14 15:34:37 +0100

    Move 'no_activity' string to it's original place

--- resources/lang/en/admin/index.php
+++ resources/lang/en/admin/index.php
@@ -15,8 +15,8 @@ return [
 
     'help-btn' => 'Get Help',
     'docs-btn' => 'Documentation',
-    'no_activity' => 'There is no recent activity to display.',
     'metrics-header' => 'User Activity Metrics',
     'most-active-country' => 'Most Active Country',
     'activity-description' => 'Based on recent login activity.',
+    'no_activity' => 'There is no recent activity to display.',
 ];
