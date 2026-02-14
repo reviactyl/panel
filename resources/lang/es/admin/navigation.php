fbf5f91121a6dc2fec2e4e9a72925ceab6a52cf4
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-14 07:45:16 +0530

    New translations navigation.php (Spanish)

--- resources/lang/es/admin/navigation.php
+++ resources/lang/es/admin/navigation.php
@@ -8,6 +8,8 @@ return [
         'settings' => 'Ajustes',
         'api' => 'API de la aplicación',
         'activity_log' => 'Activity Log',
+        'user_activity_metrics' => 'User Activity Metrics',
+        'no_data' => 'No data available.',
     ],
     'management' => [
         'title' => 'Gestión',
