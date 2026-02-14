4b45f25574be63d779f8b6ac39baf637006c9408
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-14 07:45:27 +0530

    New translations navigation.php (Turkish)

--- resources/lang/tr/admin/navigation.php
+++ resources/lang/tr/admin/navigation.php
@@ -8,6 +8,8 @@ return [
         'settings' => 'Ayarlar',
         'api' => 'Uygulama API',
         'activity_log' => 'Activity Log',
+        'user_activity_metrics' => 'User Activity Metrics',
+        'no_data' => 'No data available.',
     ],
     'management' => [
         'title' => 'Kaynak Yönetimi',
