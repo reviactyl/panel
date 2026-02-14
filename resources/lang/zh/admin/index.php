a01cbd322ae44fd9c5a9804a38828bbf75263054
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-14 07:45:11 +0530

    New translations index.php (Chinese Simplified)

--- resources/lang/zh/admin/index.php
+++ resources/lang/zh/admin/index.php
@@ -15,5 +15,8 @@ return [
 
     'help-btn' => '获取帮助',
     'docs-btn' => '使用文档',
+    'metrics-header' => 'User Activity Metrics',
+    'most-active-country' => 'Most Active Country',
+    'activity-description' => 'Based on recent login activity.',
     'no_activity' => 'There is no recent activity to display.',
 ];
