cea19652d8320395a42765b9d60e575de59168ae
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-14 07:45:15 +0530

    New translations navigation.php (French)

--- resources/lang/fr/admin/navigation.php
+++ resources/lang/fr/admin/navigation.php
@@ -8,6 +8,8 @@ return [
         'settings' => 'Paramètres',
         'api' => 'API',
         'activity_log' => 'Activity Log',
+        'user_activity_metrics' => 'User Activity Metrics',
+        'no_data' => 'No data available.',
     ],
     'management' => [
         'title' => 'Gestion',
