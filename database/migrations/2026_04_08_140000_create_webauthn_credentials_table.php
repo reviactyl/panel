03d803857bbb422448bc25d0d84acb76bd45da7b
jasonzli <jasonzxl@icloud.com>
2026-04-09 01:33:20 +0000

    db migration

---
+++ database/migrations/2026_04_08_140000_create_webauthn_credentials_table.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Laragear\WebAuthn\Models\WebAuthnCredential;
+
+return WebAuthnCredential::migration()->with(function (Blueprint $table) {
+    // Reserved for future passkey use.
+});
