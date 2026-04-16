15921cf2d7dc86dc156fb9f1eb656e35394fa7b3
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-04-16 21:33:39 +0530

    use codemirror as default

--- resources/scripts/components/dashboard/AccountOverviewContainer.tsx
+++ resources/scripts/components/dashboard/AccountOverviewContainer.tsx
@@ -24,7 +24,6 @@ import LanguageSwitcher from '@/reviactyl/ui/LanguageSwitcher';
 import { InvertToggle } from '@/reviactyl/ui/SmartInvert';
 import useFlash from '@/plugins/useFlash';
 import FlashMessageRender from '@/components/FlashMessageRender';
-import FileEditorSwitcher from '@/reviactyl/ui/FileEditorSwitcher';
 
 import SocialLoginsContainer from '@/components/dashboard/forms/SocialLoginsContainer';
 import { ExtensionSlot } from '@/extensions/ExtensionSlot';
@@ -148,7 +147,6 @@ export default () => {
                     <TitledGreyBox title={t('overview.customization')}>
                         <LanguageSwitcher />
                         <InvertToggle />
-                        <FileEditorSwitcher />
                     </TitledGreyBox>
                     <TitledGreyBox title={t('overview.2fa-verification')}>
                         <ConfigureTwoFactorForm />
