6d869ea92b3fad40945002bd33a7f5ea83cde638
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-04-22 22:06:28 +0530

    New translations schedules.php (Vietnamese)

---
+++ resources/lang/vi/server/schedules.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Schedules',
+    'no-schedules' => 'There are no schedules configured for this server.',
+    'create-schedule' => 'Create Schedule',
+    'last-run-at' => 'Last run at: :time',
+    'never' => 'never',
+    'active' => 'Active',
+    'inactive' => 'Inactive',
+    'processing' => 'Processing',
+];
