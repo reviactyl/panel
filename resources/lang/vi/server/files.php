2cfb1d814e40e3882d55bae5e36b5ec0828d74fa
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-04-22 22:06:22 +0530

    New translations files.php (Vietnamese)

---
+++ resources/lang/vi/server/files.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'title' => 'File Manager',
+    'new-file' => 'New File',
+    'empty' => 'This directory seems to be empty.',
+    'no-results' => 'No files matched your search.',
+    'too-large' => 'This directory is too large to display in the browser, limiting the output to the first 250 files.',
+    'move' => 'Move',
+    'archive' => 'Archive',
+    'delete' => 'Delete',
+    'create-directory' => 'Create Directory',
+    'file-name-label' => 'File Name',
+    'file-name-description' => 'Enter the name that this file should be saved as.',
+    'create-file' => 'Create File',
+    'file-mode-label' => 'File Mode',
+    'update' => 'Update',
+    'url-download' => [
+        'button' => 'URL Download',
+        'url-label' => 'File URL',
+        'url-description' => 'Enter a direct HTTPS URL to a file. The file will be downloaded to the current directory by Wings.',
+        'download' => 'Download',
+        'cancel' => 'Cancel',
+        'url-required' => 'A URL is required.',
+        'url-not-https' => 'Only HTTPS URLs are allowed.',
+        'url-injection' => 'The URL contains invalid characters.',
+        'url-invalid' => 'Please enter a valid URL.',
+    ],
+    'search' => [
+           'placeholder' => 'Search files...',
+           'button' => 'Search',
+           'clear' => 'Clear search',
+    ],
+];
