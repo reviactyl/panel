2f80306c02e83a747362506a74b056f226c8af02
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-04-26 21:59:41 +0530

    add errors preview to new designify

--- routes/base.php
+++ routes/base.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Base;
+use App\Http\Middleware\AdminAuthenticate;
 use App\Http\Middleware\RequireTwoFactorAuthentication;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +23,13 @@ Route::get('/manifest.json', [Base\PwaManifestController::class, 'index'])
 Route::get('/status/{server}', [Base\IndexController::class, 'index'])
     ->withoutMiddleware(['auth', 'auth.session', RequireTwoFactorAuthentication::class]);
 
+Route::prefix('preview')
+    ->middleware(['auth', AdminAuthenticate::class])
+    ->group(function () {
+        Route::get('/404', fn () => response()->view('errors.404', [], 404));
+        Route::get('/403', fn () => response()->view('errors.403', [], 403));
+        Route::get('/500', fn () => response()->view('errors.500', [], 500));
+    });
+
 Route::get('/{react}', [Base\IndexController::class, 'index'])
-    ->where('react', '^(?!(\/)?(api|auth|admin|panel|designify|daemon)).+');
+    ->where('react', '^(?!(\/)?(api|auth|admin|preview|designify|daemon)).+');
