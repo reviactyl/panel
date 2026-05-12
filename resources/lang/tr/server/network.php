923de144ae5632a4a37e06f1d805a1f277a11cde
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:39:25 +0530

    New translations network.php (Turkish)
    [ci skip]

--- resources/lang/tr/server/network.php
+++ resources/lang/tr/server/network.php
@@ -4,4 +4,7 @@ return [
     'title' => 'Ağ',
     'created' => 'Şu anda bu sunucu için izin verilen {{limit}} haktan {{count}} tahsisi kullanıyorsunuz.',
     'create' => 'Tahsis Oluştur',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
 ];
