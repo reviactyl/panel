b14cbc48d0b0e989df15c5c66a7056d04a88bf8c
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:41:54 +0530

    New translations network.php (Chinese Simplified)
    [ci skip]

--- resources/lang/zh/server/network.php
+++ resources/lang/zh/server/network.php
@@ -4,4 +4,7 @@ return [
     'title' => '网络',
     'created' => '您当前已使用 {{count}} 个分配项，共可使用 {{limit}} 个。',
     'create' => '创建分配',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
 ];
