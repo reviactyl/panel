039785172e22230fc6616fa44882511fb970adf3
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:39:46 +0530

    New translations network.php (Portuguese, Brazilian)
    [ci skip]

--- resources/lang/pt/server/network.php
+++ resources/lang/pt/server/network.php
@@ -4,4 +4,7 @@ return [
     'title' => 'Rede',
     'created' => 'Você está utilizando atualmente {{count}} de {{limit}} alocações permitidas para este servidor.',
     'create' => 'Criar Alocação',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
 ];
