5497d48124ff99d1a863c99ba3ca3e45904e6320
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:37:59 +0530

    New translations network.php (German)
    [ci skip]

--- resources/lang/de/server/network.php
+++ resources/lang/de/server/network.php
@@ -4,4 +4,7 @@ return [
     'title' => 'Netzwerk',
     'created' => 'Sie verwenden derzeit {{count}} von {{limit}} erlaubten Zuweisungen für diesen Server.',
     'create' => 'Zuweisung erstellen',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
 ];
