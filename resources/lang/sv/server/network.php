238648c00bdd169ba2f5acb57f3ba189e1ba54d9
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:39:04 +0530

    New translations network.php (Swedish)
    [ci skip]

--- resources/lang/sv/server/network.php
+++ resources/lang/sv/server/network.php
@@ -4,4 +4,7 @@ return [
     'title' => 'Nätverk',
     'created' => 'Du använder för närvarande {{count}} av {{limit}} tillåtna allokeringar för denna server.',
     'create' => 'Skapa allokering',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
 ];
