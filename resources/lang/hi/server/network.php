1fe29ce7c00a3790c33ebf3915352cdb5bf92a09
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:38:21 +0530

    New translations network.php (Hindi)
    [ci skip]

--- resources/lang/hi/server/network.php
+++ resources/lang/hi/server/network.php
@@ -4,4 +4,7 @@ return [
     'title' => 'नेटवर्क',
     'created' => 'आप वर्तमान में इस सर्वर के लिए अनुमत {{limit}} में से {{count}} एलोकेशन का उपयोग कर रहे हैं।',
     'create' => 'एलोकेशन बनाएं',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
 ];
