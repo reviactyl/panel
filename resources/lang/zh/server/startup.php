36d931f0da5ab2e950f4789faf478e1744d08468
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:41:58 +0530

    New translations startup.php (Chinese Simplified)
    [ci skip]

--- resources/lang/zh/server/startup.php
+++ resources/lang/zh/server/startup.php
@@ -2,6 +2,7 @@
 
 return [
     'title' => '启动',
+    'read-only' => 'Read Only',
     'startup-command' => '启动命令',
     'docker-image' => 'Docker 镜像',
     'docker-info' => '这是一个高级功能，允许您选择 Docker 镜像以在运行此服务器实例时使用。',
