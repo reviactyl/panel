7967865078d78cfc3f165edb1916a417bc00bf72
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-12 13:41:19 +0530

    New translations network.php (Arabic)
    [ci skip]

--- resources/lang/ar/server/network.php
+++ resources/lang/ar/server/network.php
@@ -4,4 +4,7 @@ return [
     'title' => 'شبكة',
     'created' => 'أنت تستخدم حاليًا {{count}} من {{limit}} التخصيصات المسموح بها لهذا الخادم.',
     'create' => 'إنشاء التخصيص',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
 ];
