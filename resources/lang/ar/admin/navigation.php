8d8190e3b4a0f3040f9bcfdb24ebd2d4ea0b7d8e
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-13 10:55:55 +0530

    New translations navigation.php (Arabic)
    [ci skip]

--- resources/lang/ar/admin/navigation.php
+++ resources/lang/ar/admin/navigation.php
@@ -21,7 +21,7 @@ return [
     ],
     'service' => [
         'title' => 'إدارة السيرفرات',
-        'extensions' => 'الإضافات',
+        'extensions' => 'Extensions',
         'mounts' => 'التوصيلات',
         'nests' => 'الأعشاش',
     ],
