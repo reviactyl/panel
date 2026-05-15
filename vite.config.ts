f925a05574f534c671440e77d1790a144a4657f6
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-05-15 15:48:21 +0530

    ignore app directories

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
 
     server: {
         watch: {
-            ignored: ['**/vendor/**'],
+            ignored: ['**/vendor/**', '**/storage/**', '**/bootstrap/**', '**/resources/views/**', '**/database/**'],
         },
     },
     
