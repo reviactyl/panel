b0d5bb78e5c1c916dbfaa9b890634a2cce00d328
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-16 18:58:22 +0530

    New translations network.php (Bengali)
    [ci skip]

---
+++ resources/lang/bn/server/network.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Network',
+    'created' => 'You are currently using {{count}} of {{limit}} allowed allocations for this server.',
+    'create' => 'Create Allocation',
+    'delete-allocation-title' => 'Remove Allocation',
+    'delete-allocation-confirm' => 'Delete',
+    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
+];
