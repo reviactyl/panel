aefd01656da630099cb303ded30f6e06faefe493
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-05-26 14:13:14 +0530

    New translations generic.php (Bengali)
    [ci skip]

--- resources/lang/bn/generic.php
+++ resources/lang/bn/generic.php
@@ -2,7 +2,7 @@
 
 return [
     'never' => 'কখনও নয়',
-    'not-found-title' => '404',
+    'not-found-title' => '৪০৪',
     'not-found-message' => 'অনুরোধকৃত রিসোর্সটি পাওয়া যায়নি।',
     'server-error-title' => 'কিছু একটা ভুল হয়েছে',
 ];
