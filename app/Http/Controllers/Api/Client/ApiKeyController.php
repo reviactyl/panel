a0f6cfb0eb01a3e69045690e7a4d5d81d7a6d608
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-06-13 15:56:17 +0530

    revert changes to apikey controller

--- app/Http/Controllers/Api/Client/ApiKeyController.php
+++ app/Http/Controllers/Api/Client/ApiKeyController.php
@@ -9,7 +9,6 @@ use App\Http\Requests\Api\Client\ClientApiRequest;
 use App\Models\ApiKey;
 use App\Transformers\Api\Client\ApiKeyTransformer;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 
 class ApiKeyController extends ClientApiController
 {
@@ -30,16 +29,14 @@ class ApiKeyController extends ClientApiController
      */
     public function store(StoreApiKeyRequest $request): array
     {
-        $token = DB::transaction(function () use ($request) {
-            if ($request->user()->apiKeys()->lockForUpdate()->count() >= 25) {
-                throw new DisplayException('You have reached the account limit for number of API keys.');
-            }
+        if ($request->user()->apiKeys->count() >= 25) {
+            throw new DisplayException('You have reached the account limit for number of API keys.');
+        }
 
-            return $request->user()->createToken(
-                $request->input('description'),
-                $request->input('allowed_ips')
-            );
-        });
+        $token = $request->user()->createToken(
+            $request->input('description'),
+            $request->input('allowed_ips')
+        );
 
         Activity::event('user:api-key.create')
             ->subject($token->accessToken)
