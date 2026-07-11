151bad908013a4ee22ab3b44fbbe9e09117ebdf7
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-07-12 04:28:50 +0530

    New translations mounts.php (Chinese Traditional)
    [ci skip]

--- resources/lang/zh/admin/mounts.php
+++ resources/lang/zh/admin/mounts.php
@@ -2,43 +2,43 @@
 
 return [
 
-    'label' => '挂载',
-    'plural_label' => '挂载',
+    'label' => 'Mount',
+    'plural_label' => 'Mounts',
 
     'sections' => [
-        'configuration' => '挂载配置',
+        'configuration' => 'Mount Configuration',
     ],
 
     'fields' => [
-        'name' => '名称',
-        'description' => '描述',
-        'source' => '来源路径',
-        'target' => '目标路径',
-        'read_only' => '只读',
-        'user_mountable' => '允许用户挂载',
+        'name' => 'Name',
+        'description' => 'Description',
+        'source' => 'Source Path',
+        'target' => 'Target Path',
+        'read_only' => 'Read Only',
+        'user_mountable' => 'User Mountable',
     ],
 
     'helpers' => [
-        'name' => '用于区分此挂载与其他挂载的唯一名称。',
-        'description' => '便于人工阅读的此挂载的详细说明。',
-        'source' => '主机上要挂载到容器中的文件路径。',
-        'target' => '容器内部将此路径挂载到的目标位置。',
-        'read_only' => '如果勾选，则容器内部此挂载点将为只读。',
-        'user_mountable' => '如果勾选，用户将可以在自己的服务器中挂载此挂载点。',
+        'name' => 'A unique name used to separate this mount from another.',
+        'description' => 'A longer, human-readable description of this mount.',
+        'source' => 'The file path on the host machine to mount to containers.',
+        'target' => 'The path inside the container to mount this as.',
+        'read_only' => 'If set, the mount will be read-only inside the container.',
+        'user_mountable' => 'If set, users will be able to mount this to their servers.',
     ],
 
     'columns' => [
         'id' => 'ID',
-        'name' => '名称',
-        'source' => '源路径',
-        'target' => '目标',
-        'read_only' => '只读',
-        'user_mountable' => '允许用户挂载',
+        'name' => 'Name',
+        'source' => 'Source',
+        'target' => 'Target',
+        'read_only' => 'Read Only',
+        'user_mountable' => 'User Mountable',
     ],
 
     'actions' => [
-        'attach_egg' => '附上鸡蛋',
-        'attach_node' => '附加节点',
+        'attach_egg' => 'Attach Egg',
+        'attach_node' => 'Attach Node',
     ],
 
 ];
