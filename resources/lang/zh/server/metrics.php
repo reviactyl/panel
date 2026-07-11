3ae6b3e2e0f48e9efaeeb780474e067c1b376f42
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-07-12 04:29:13 +0530

    New translations metrics.php (Chinese Traditional)
    [ci skip]

--- resources/lang/zh/server/metrics.php
+++ resources/lang/zh/server/metrics.php
@@ -1,30 +1,30 @@
 <?php
 
 return [
-    'title' => '历史指标',
-    'error' => '无法加载历史统计数据。',
+    'title' => 'Historical Metrics',
+    'error' => 'Unable to load historical stats.',
     'time_range' => [
-        'last_24_hours' => '过去 24 小时',
-        'last_3_days' => '过去 3 天',
-        'last_7_days' => '过去 7 天',
+        'last_24_hours' => 'Last 24 Hours',
+        'last_3_days' => 'Last 3 Days',
+        'last_7_days' => 'Last 7 Days',
     ],
     'charts' => [
         'cpu' => [
-            'title' => 'CPU 历史',
-            'label' => 'CPU 使用率（%）',
+            'title' => 'CPU History',
+            'label' => 'CPU Usage (%)',
         ],
         'memory' => [
-            'title' => '内存历史',
-            'label' => '内存使用量(MB)',
+            'title' => 'Memory History',
+            'label' => 'Memory Usage (MB)',
         ],
         'disk' => [
-            'title' => '磁盘历史',
-            'label' => '磁盘使用量(MB)',
+            'title' => 'Disk History',
+            'label' => 'Disk Usage (MB)',
         ],
         'network' => [
-            'title' => '网络历史',
-            'rx_label' => '网络接收量(MB)',
-            'tx_label' => '网络发送量(MB)',
+            'title' => 'Network History',
+            'rx_label' => 'Network RX (MB)',
+            'tx_label' => 'Network TX (MB)',
         ],
     ],
 ];
