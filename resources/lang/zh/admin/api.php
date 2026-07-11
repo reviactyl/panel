812b11136f22769c0da0e92201d6f16eabf796ce
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-07-12 04:28:42 +0530

    New translations api.php (Chinese Traditional)
    [ci skip]

--- resources/lang/zh/admin/api.php
+++ resources/lang/zh/admin/api.php
@@ -1,28 +1,28 @@
 <?php
 
 return [
-    'title' => '应用程序 API',
-    'list-title' => '凭据列表',
-    'key' => '密钥',
-    'memo' => '备注',
-    'last-used' => '最后使用',
-    'created' => '创建于',
-    'author' => '作者',
-    'never-used' => '从未使用',
+    'title' => 'Application API',
+    'list-title' => 'Credentials List',
+    'key' => 'Key',
+    'memo' => 'Memo',
+    'last-used' => 'Last Used',
+    'created' => 'Created On',
+    'author' => 'Author',
+    'never-used' => 'Never Used',
 
-    'revoke' => '撤销',
-    'revoke-title' => '撤销 API 密钥',
-    'revoke-warning' => '一旦撤销此 API 密钥，当前使用该密钥的所有应用将无法继续工作。',
-    'revoked' => 'API 密钥已被撤销。',
+    'revoke' => 'Revoke',
+    'revoke-title' => 'Revoke API Key',
+    'revoke-warning' => 'Once this API key is revoked any applications currently using it will stop working.',
+    'revoked' => 'API Key has been revoked.',
 
-    'permissions' => '选择权限',
-    'read-only' => '只读',
-    'read-write' => '读写',
-    'none' => '无',
+    'permissions' => 'Select Permissions',
+    'read-only' => 'Read',
+    'read-write' => 'Read & Write',
+    'none' => 'None',
 
-    'description' => '描述',
-    'info' => '一旦分配权限并创建了这组凭据，您将无法返回进行编辑。如需在之后进行更改，必须创建一组新的凭据。',
+    'description' => 'Description',
+    'info' => 'Once you have assigned permissions and created this set of credentials you will be unable to come back and edit it. If you need to make changes down the road you will need to create a new set of credentials.',
 
-    'create-btn' => '创建新的',
-    'new-btn' => '创建凭据',
+    'create-btn' => 'Create New',
+    'new-btn' => 'Create Credentials',
 ];
