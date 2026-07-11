6744f5b38fd96b83ca2a41730f0bab81d55cbfa1
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-07-12 04:29:03 +0530

    New translations generic.php (Chinese Traditional)
    [ci skip]

--- resources/lang/zh/generic.php
+++ resources/lang/zh/generic.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'never' => '从不',
+    'never' => 'Never',
     'not-found-title' => '404',
-    'not-found-message' => '未找到请求的资源。',
-    'server-error-title' => '出了点问题',
+    'not-found-message' => 'The requested resource was not found.',
+    'server-error-title' => 'Something went wrong',
 ];
