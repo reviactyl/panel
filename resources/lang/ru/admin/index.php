69ce08747f0fd7425aa485782bc4a6d9a7a355dd
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 20:10:58 +0530

    New translations index.php (Russian)
    [ci skip]

--- resources/lang/ru/admin/index.php
+++ resources/lang/ru/admin/index.php
@@ -22,4 +22,13 @@ return [
     'no_activity' => 'There is no recent activity to display.',
     'activity-header' => 'Recent Activity',
     'more-btn' => 'View More',
+    'dashboard' => [
+        'total_users' => 'Total Users',
+        'new_users' => 'New users: :count',
+        'total_servers' => 'Total Servers',
+        'new_servers' => 'New servers: :count',
+        'developmental_build' => 'Developmental Build',
+        'up_to_date' => 'Up-to-date',
+        'outdated_build' => 'Outdated Build',
+    ],
 ];
