269ac81f40b765343fa5c8b5aceec1ad4adb359c
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 11:02:29 +0530

    New translations installer.php (Russian)
    [ci skip]

--- resources/lang/ru/installer.php
+++ resources/lang/ru/installer.php
@@ -2,82 +2,82 @@
 
 return [
     'meta' => [
-        'title' => 'Welcome to Reviactyl',
+        'title' => 'Добро пожаловать в Reviactyl',
     ],
 
     'language' => [
-        'label' => 'Language',
+        'label' => 'Язык',
     ],
 
     'migration' => [
-        'title' => 'Migration in progress',
-        'message' => 'Do not refresh or exit this page...',
+        'title' => 'Продолжается миграция',
+        'message' => 'Не обновляйте эту страницу и не покидайте ее...',
     ],
 
     'progress' => [
-        'step' => 'Step :step',
+        'step' => 'Шаг :step',
     ],
 
     'brand' => [
-        'title' => 'Reviactyl Panel (Fission Falcon)',
+        'title' => 'Панель Reviactyl (Fission Falcon)',
         'art_alt' => 'Fission Falcon Artwork',
     ],
 
     'welcome' => [
-        'message' => 'To proceed with the installation, click the button below.',
+        'message' => 'Чтобы продолжить установку, нажмите на кнопку ниже.',
     ],
 
     'finish' => [
-        'message' => 'To finish the installation, click the button below.',
+        'message' => 'Чтобы завершить установку, нажмите на кнопку ниже.',
     ],
 
     'sections' => [
-        'application' => 'Application Configuration',
-        'database' => 'Database Configuration',
-        'user' => 'User Configuration',
-        'mail' => 'Mail Settings',
+        'application' => 'Конфигурация приложения',
+        'database' => 'Конфигурация базы данных',
+        'user' => 'Конфигурация пользователя',
+        'mail' => 'Настройки почты',
     ],
 
     'fields' => [
-        'application_url' => 'Application URL',
-        'cache_driver' => 'Cache driver',
-        'database_name' => 'Database name',
-        'driver' => 'Driver',
-        'email' => 'Email',
-        'enable_settings_editor' => 'Enable settings editor',
-        'encryption' => 'Encryption',
-        'endpoint' => 'Endpoint',
-        'first_name' => 'First name',
-        'from_email' => 'From email',
-        'from_name' => 'From name',
-        'host' => 'Host',
-        'host_domain' => 'Host / Domain',
-        'icon_url' => 'Icon URL',
-        'last_name' => 'Last name',
-        'logo_url' => 'Logo URL',
-        'password' => 'Password',
-        'password_secret' => 'Password / Secret',
-        'port' => 'Port',
-        'queue_driver' => 'Queue driver',
-        'redis_host' => 'Redis host',
-        'redis_password' => 'Redis password',
-        'redis_port' => 'Redis port',
-        'send_telemetry' => 'Send anonymous telemetry',
-        'service_author_email' => 'Service author email',
-        'session_driver' => 'Session driver',
-        'sqlite_database_path' => 'SQLite database path',
-        'timezone' => 'Timezone',
-        'username' => 'Username',
+        'application_url' => 'URL-адрес приложения',
+        'cache_driver' => 'Драйвер кэша',
+        'database_name' => 'Имя базы данных',
+        'driver' => 'Драйвер',
+        'email' => 'Электронная почта',
+        'enable_settings_editor' => 'Включить редактор настроек',
+        'encryption' => 'Шифрование',
+        'endpoint' => 'Конечная точка',
+        'first_name' => 'Имя',
+        'from_email' => 'Из электронной почты',
+        'from_name' => 'От имени',
+        'host' => 'Хост',
+        'host_domain' => 'Хост / Домен',
+        'icon_url' => 'URL-адрес значка',
+        'last_name' => 'Фамилия',
+        'logo_url' => 'URL-адрес лого',
+        'password' => 'Пароль',
+        'password_secret' => 'Пароль / Секрет',
+        'port' => 'Порт',
+        'queue_driver' => 'Драйвер очереди',
+        'redis_host' => 'Хост Redis',
+        'redis_password' => 'Redis пароль',
+        'redis_port' => 'Redis порт',
+        'send_telemetry' => 'Отправка анонимной телеметрии',
+        'service_author_email' => 'Электронная почта автора сервиса',
+        'session_driver' => 'Драйвер сеанса',
+        'sqlite_database_path' => 'Путь к базе данных SQLite',
+        'timezone' => 'Часовой пояс',
+        'username' => 'Логин',
     ],
 
     'options' => [
-        'none' => 'None',
+        'none' => 'Ничего',
     ],
 
     'actions' => [
-        'continue' => 'Continue',
-        'finish' => 'Finish Installation',
-        'proceed' => 'Proceed...',
-        'skip' => 'Skip...',
+        'continue' => 'Продолжить',
+        'finish' => 'Завершите установку',
+        'proceed' => 'Приступить...',
+        'skip' => 'Пропустить...',
     ],
 ];
