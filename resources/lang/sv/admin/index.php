d5764babacfc8253e982e5d67c3ad43919979c23
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 20:10:59 +0530

    New translations index.php (Swedish)
    [ci skip]

--- resources/lang/sv/admin/index.php
+++ resources/lang/sv/admin/index.php
@@ -22,4 +22,13 @@ return [
     'no_activity' => 'Det finns ingen ny aktivitet att visa.',
     'activity-header' => 'Senaste aktivitet',
     'more-btn' => 'Visa mer',
+    'dashboard' => [
+        'total_users' => 'Total Users',
+        'new_users' => 'New users: :count',
+        'total_servers' => 'Total Servers',
+        'new_servers' => 'New servers: :count',
+        'developmental_build' => 'Developmental Build',
+        'up_to_date' => 'Up-to-date',
+        'outdated_build' => 'Outdated Build',
+    ],
 ];
