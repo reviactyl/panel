f6693adfd92d18c3432590167b5f24df1af18cb8
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 10:58:25 +0530

    New translations index.php (Spanish)
    [ci skip]

--- resources/lang/es/dashboard/index.php
+++ resources/lang/es/dashboard/index.php
@@ -39,6 +39,7 @@ return [
         'restoring-backup' => 'Restaurar copia de seguridad',
         'unavailable' => 'No disponible',
         'unlimited' => 'Ilimitado',
+        'maintenance' => 'Under Maintenance',
     ],
     'status-card' => [
         'title' => 'Estado del servidor',
