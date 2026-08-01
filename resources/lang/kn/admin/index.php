4efd4dd90b0f4ed63dabef3d01bc15c5bac778dd
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 20:11:09 +0530

    New translations index.php (Kannada)
    [ci skip]

--- resources/lang/kn/admin/index.php
+++ resources/lang/kn/admin/index.php
@@ -22,4 +22,13 @@ return [
     'no_activity' => 'ಪ್ರದರ್ಶಿಸಲು ಯಾವುದೇ ಇತ್ತೀಚಿನ ಚಟುವಟಿಕೆ ಇಲ್ಲ.',
     'activity-header' => 'ಇತ್ತೀಚಿನ ಚಟುವಟಿಕೆ',
     'more-btn' => 'ಇನ್ನಷ್ಟು ವೀಕ್ಷಿಸಿ',
+    'dashboard' => [
+        'total_users' => 'Total Users',
+        'new_users' => 'New users: :count',
+        'total_servers' => 'Total Servers',
+        'new_servers' => 'New servers: :count',
+        'developmental_build' => 'Developmental Build',
+        'up_to_date' => 'Up-to-date',
+        'outdated_build' => 'Outdated Build',
+    ],
 ];
