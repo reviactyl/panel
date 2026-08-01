c9d994c2ad9ff65d098a530a74657aebc2554bc0
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 12:58:32 +0530

    New translations files.php (Bengali)
    [ci skip]

--- resources/lang/bn/server/files.php
+++ resources/lang/bn/server/files.php
@@ -22,6 +22,7 @@ return [
     'file-mode-label' => 'ফাইল মোড',
     'new-location' => 'নতুন অবস্থান:',
     'cancel-uploads' => 'আপলোড বাতিল করুন',
+    'close' => 'Close',
     'uploads-tooltip' => ':count টি ফাইল আপলোড হচ্ছে, দেখতে ক্লিক করুন',
     'move-description' => 'বর্তমান ডিরেক্টরির তুলনায় এই ফাইল বা ফোল্ডারের নতুন নাম এবং ডিরেক্টরি লিখুন।',
     'update' => 'আপডেট',
