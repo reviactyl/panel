015f1ef3138dc4650d48c1c8a7e7fb5110c28e89
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 20:10:54 +0530

    New translations index.php (French)
    [ci skip]

--- resources/lang/fr/admin/index.php
+++ resources/lang/fr/admin/index.php
@@ -22,4 +22,13 @@ return [
     'no_activity' => 'Aucune activité récente à afficher.',
     'activity-header' => 'Activité récente',
     'more-btn' => 'Voir plus',
+    'dashboard' => [
+        'total_users' => 'Total Users',
+        'new_users' => 'New users: :count',
+        'total_servers' => 'Total Servers',
+        'new_servers' => 'New servers: :count',
+        'developmental_build' => 'Developmental Build',
+        'up_to_date' => 'Up-to-date',
+        'outdated_build' => 'Outdated Build',
+    ],
 ];
