a36de43569928644a75c28ac23651c74ae7347b0
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 20:11:03 +0530

    New translations index.php (Vietnamese)
    [ci skip]

--- resources/lang/vi/admin/index.php
+++ resources/lang/vi/admin/index.php
@@ -22,4 +22,13 @@ return [
     'no_activity' => 'Không có hoạt động gần đây để hiển thị.',
     'activity-header' => 'Hoạt động gần đây',
     'more-btn' => 'Xem thêm',
+    'dashboard' => [
+        'total_users' => 'Total Users',
+        'new_users' => 'New users: :count',
+        'total_servers' => 'Total Servers',
+        'new_servers' => 'New servers: :count',
+        'developmental_build' => 'Developmental Build',
+        'up_to_date' => 'Up-to-date',
+        'outdated_build' => 'Outdated Build',
+    ],
 ];
