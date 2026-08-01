4e9d5ff6eeef543ef0a17d828ecd72165b15de86
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 11:00:54 +0530

    New translations network.php (Russian)
    [ci skip]

--- resources/lang/ru/server/network.php
+++ resources/lang/ru/server/network.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'title' => 'Network',
-    'created' => 'You are currently using {{count}} of {{limit}} allowed allocations for this server.',
-    'create' => 'Create Allocation',
-    'delete-allocation-title' => 'Remove Allocation',
-    'delete-allocation-confirm' => 'Delete',
-    'delete-allocation-message' => 'This allocation will be immediately removed from your server.',
+    'title' => 'Сеть',
+    'created' => 'В настоящее время для этого сервера используется {{count}} из {{limit}} доступных портов.',
+    'create' => 'Создать порт',
+    'delete-allocation-title' => 'Удалить распределение',
+    'delete-allocation-confirm' => 'Удалить',
+    'delete-allocation-message' => 'Это распределение будет немедленно удалено с вашего сервера.',
 ];
