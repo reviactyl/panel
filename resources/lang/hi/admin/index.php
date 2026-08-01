2cf3e2931e56693f1c06a9faa60af272c7afe6e9
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 20:11:07 +0530

    New translations index.php (Hindi)
    [ci skip]

--- resources/lang/hi/admin/index.php
+++ resources/lang/hi/admin/index.php
@@ -22,4 +22,13 @@ return [
     'no_activity' => 'प्रदर्शित करने के लिए कोई हालिया गतिविधि नहीं है.',
     'activity-header' => 'हाल की गतिविधि',
     'more-btn' => 'और देखें',
+    'dashboard' => [
+        'total_users' => 'Total Users',
+        'new_users' => 'New users: :count',
+        'total_servers' => 'Total Servers',
+        'new_servers' => 'New servers: :count',
+        'developmental_build' => 'Developmental Build',
+        'up_to_date' => 'Up-to-date',
+        'outdated_build' => 'Outdated Build',
+    ],
 ];
