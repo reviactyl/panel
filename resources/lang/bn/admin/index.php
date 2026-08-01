48d8b6163a3490a7d31a3ffe87686745ee3abd23
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 20:11:06 +0530

    New translations index.php (Bengali)
    [ci skip]

--- resources/lang/bn/admin/index.php
+++ resources/lang/bn/admin/index.php
@@ -22,4 +22,13 @@ return [
     'no_activity' => 'দেখানোর মতো কোনো সাম্প্রতিক কার্যকলাপ নেই।',
     'activity-header' => 'সাম্প্রতিক কার্যকলাপ',
     'more-btn' => 'আরও দেখুন',
+    'dashboard' => [
+        'total_users' => 'Total Users',
+        'new_users' => 'New users: :count',
+        'total_servers' => 'Total Servers',
+        'new_servers' => 'New servers: :count',
+        'developmental_build' => 'Developmental Build',
+        'up_to_date' => 'Up-to-date',
+        'outdated_build' => 'Outdated Build',
+    ],
 ];
