4161891d3f05043caa65743af549082fd6f81f49
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 10:55:32 +0530

    New translations api.php (Russian)
    [ci skip]

--- resources/lang/ru/admin/api.php
+++ resources/lang/ru/admin/api.php
@@ -1,28 +1,28 @@
 <?php
 
 return [
-    'title' => 'Application API',
-    'list-title' => 'Credentials List',
-    'key' => 'Key',
-    'memo' => 'Memo',
-    'last-used' => 'Last Used',
-    'created' => 'Created On',
-    'author' => 'Author',
-    'never-used' => 'Never Used',
+    'title' => 'API приложения',
+    'list-title' => 'Список ключей',
+    'key' => 'Ключ',
+    'memo' => 'Примечание',
+    'last-used' => 'Последнее использование',
+    'created' => 'Дата создания',
+    'author' => 'Автор',
+    'never-used' => 'Никогда не использовался',
 
-    'revoke' => 'Revoke',
-    'revoke-title' => 'Revoke API Key',
-    'revoke-warning' => 'Once this API key is revoked any applications currently using it will stop working.',
-    'revoked' => 'API Key has been revoked.',
+    'revoke' => 'Отозвать',
+    'revoke-title' => 'Отозвать API ключ',
+    'revoke-warning' => 'Как только этот API-ключ будет отозван, все приложения, использующие его в настоящее время, перестанут работать.',
+    'revoked' => 'Ключ API был отозван.',
 
-    'permissions' => 'Select Permissions',
-    'read-only' => 'Read',
-    'read-write' => 'Read & Write',
-    'none' => 'None',
+    'permissions' => 'Выберите права доступа',
+    'read-only' => 'Только чтение',
+    'read-write' => 'Чтение и запись',
+    'none' => 'Нет',
 
-    'description' => 'Description',
-    'info' => 'Once you have assigned permissions and created this set of credentials you will be unable to come back and edit it. If you need to make changes down the road you will need to create a new set of credentials.',
+    'description' => 'Описание',
+    'info' => 'После выдачи прав и создания учётных данных вы не сможете их отредактировать. Если в будущем потребуется изменить права, вам нужно будет создать новый ключ.',
 
-    'create-btn' => 'Create New',
-    'new-btn' => 'Create Credentials',
+    'create-btn' => 'Создать новый',
+    'new-btn' => 'Создать учётные данные',
 ];
