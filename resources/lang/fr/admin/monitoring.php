1dffbe5992f3712e04aebf0b96d7d7613ab0a2fb
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-07 18:20:46 +0530

    New translations monitoring.php (French)
    [ci skip]

--- resources/lang/fr/admin/monitoring.php
+++ resources/lang/fr/admin/monitoring.php
@@ -61,6 +61,14 @@ return [
         'swap_free' => 'Gratuit',
         'swap_usage' => 'Usage',
 
+        'partitions_section' => 'Disk Partitions',
+        'partitions_none' => 'No partition data available.',
+        'partitions_device' => 'Device',
+        'partitions_mountpoint' => 'Mount Point',
+        'partitions_filesystem' => 'Filesystem',
+        'partitions_size' => 'Size',
+        'partitions_usage' => 'Usage',
+
         'network_section' => 'Réseau',
         'bytes_sent' => 'Octets envoyés',
         'bytes_recv' => 'Octets reçus',
