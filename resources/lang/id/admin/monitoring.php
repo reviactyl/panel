437da1f81ff42684e388b768bd579ff5b1074d62
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-07 18:20:59 +0530

    New translations monitoring.php (Indonesian)
    [ci skip]

--- resources/lang/id/admin/monitoring.php
+++ resources/lang/id/admin/monitoring.php
@@ -61,6 +61,14 @@ return [
         'swap_free' => 'Bebas',
         'swap_usage' => 'Penggunaan',
 
+        'partitions_section' => 'Disk Partitions',
+        'partitions_none' => 'No partition data available.',
+        'partitions_device' => 'Device',
+        'partitions_mountpoint' => 'Mount Point',
+        'partitions_filesystem' => 'Filesystem',
+        'partitions_size' => 'Size',
+        'partitions_usage' => 'Usage',
+
         'network_section' => 'Jaringan',
         'bytes_sent' => 'Byte Terkirim',
         'bytes_recv' => 'Byte Diterima',
