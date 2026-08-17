aaf93098e086b7598a26eb3bacafd6b49c7f6953
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-17 15:03:24 +0530

    New translations server.php (Indonesian)
    [ci skip]

--- resources/lang/id/admin/server.php
+++ resources/lang/id/admin/server.php
@@ -315,6 +315,7 @@ return [
         'bad_variable' => 'Terjadi kesalahan validasi dengan variabel :name.',
         'daemon_exception' => 'Terjadi pengecualian saat mencoba berkomunikasi dengan daemon yang menghasilkan kode respons HTTP/:code. Pengecualian ini telah dicatat. (request id: :request_id)',
         'default_allocation_not_found' => 'Alokasi default yang diminta tidak ditemukan dalam alokasi server ini.',
+        'skipping_install_script' => 'This server is configured to skip its egg\'s install script. Reinstalling is not available until that setting is disabled.',
     ],
 
     'alerts' => [
