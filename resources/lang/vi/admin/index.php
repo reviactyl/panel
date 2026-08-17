3291b53d65968f4d35cc96b489da9f826aff16c0
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-17 16:34:06 +0530

    New translations index.php (Vietnamese)
    [ci skip]

--- resources/lang/vi/admin/index.php
+++ resources/lang/vi/admin/index.php
@@ -14,7 +14,7 @@ return [
     'sponsor-btn' => 'Dự án tài trợ',
 
     'help-btn' => 'Nhận trợ giúp',
-    'update-btn' => 'Update Panel',
+    'update-btn' => 'Bảng cập nhật',
     'docs-btn' => 'Tài liệu',
     'metrics-header' => 'Số liệu hoạt động của người dùng',
     'most-active-country' => 'Quốc gia năng động nhất',
