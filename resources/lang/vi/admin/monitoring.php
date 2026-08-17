68e880d85307da5db136ad6edd2d66cdf698bae7
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-17 16:10:02 +0530

    New translations monitoring.php (Vietnamese)
    [ci skip]

--- resources/lang/vi/admin/monitoring.php
+++ resources/lang/vi/admin/monitoring.php
@@ -66,8 +66,8 @@ return [
         'partitions_device' => 'Device',
         'partitions_mountpoint' => 'Mount Point',
         'partitions_filesystem' => 'Filesystem',
-        'partitions_size' => 'Size',
-        'partitions_usage' => 'Usage',
+        'partitions_size' => 'Kích cỡ',
+        'partitions_usage' => 'Cách sử dụng',
 
         'network_section' => 'Mạng',
         'bytes_sent' => 'Byte đã gửi',
