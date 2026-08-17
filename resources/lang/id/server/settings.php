b6db5f3f6bcbb9c889c1b1f58a935118cd8f5116
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-17 15:03:25 +0530

    New translations settings.php (Indonesian)
    [ci skip]

--- resources/lang/id/server/settings.php
+++ resources/lang/id/server/settings.php
@@ -29,5 +29,6 @@ return [
         'info-1' => 'Menginstal ulang server Anda akan menghentikannya, lalu menjalankan kembali skrip instalasi yang awalnya mengaturnya.',
         'info-2' => 'Beberapa file mungkin dihapus atau dimodifikasi selama proses ini, harap backup data Anda sebelum melanjutkan.',
         'button' => 'Instal Ulang Server',
+        'disabled' => 'Reinstalling is not available because this server is configured to skip its egg\'s install script.',
     ],
 ];
