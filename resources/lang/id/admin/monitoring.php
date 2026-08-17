a8da6ae39a6736cf2b5655eb3318be2bf89e2023
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-17 16:10:05 +0530

    New translations monitoring.php (Indonesian)
    [ci skip]

--- resources/lang/id/admin/monitoring.php
+++ resources/lang/id/admin/monitoring.php
@@ -66,8 +66,8 @@ return [
         'partitions_device' => 'Device',
         'partitions_mountpoint' => 'Mount Point',
         'partitions_filesystem' => 'Filesystem',
-        'partitions_size' => 'Size',
-        'partitions_usage' => 'Usage',
+        'partitions_size' => 'Ukuran',
+        'partitions_usage' => 'Penggunaan',
 
         'network_section' => 'Jaringan',
         'bytes_sent' => 'Byte Terkirim',
