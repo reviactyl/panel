dd2447ea865fdeaccce3ad0a725f6eec1f4e991b
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-17 16:09:59 +0530

    New translations monitoring.php (Turkish)
    [ci skip]

--- resources/lang/tr/admin/monitoring.php
+++ resources/lang/tr/admin/monitoring.php
@@ -66,8 +66,8 @@ return [
         'partitions_device' => 'Device',
         'partitions_mountpoint' => 'Mount Point',
         'partitions_filesystem' => 'Filesystem',
-        'partitions_size' => 'Size',
-        'partitions_usage' => 'Usage',
+        'partitions_size' => 'Boyut',
+        'partitions_usage' => 'Kullanım',
 
         'network_section' => 'Ağ',
         'bytes_sent' => 'Gönderilen Bayt',
