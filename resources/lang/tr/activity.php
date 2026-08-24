853b91f45a6733281f1f768ba47688f190c448ce
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-08-24 22:23:29 +0530

    New translations activity.php (Turkish)
    [ci skip]

--- resources/lang/tr/activity.php
+++ resources/lang/tr/activity.php
@@ -38,6 +38,7 @@ return [
             'password-changed' => 'Parola değiştirildi',
             'language-changed' => 'Dil :old dilinden :new diline değiştirildi',
             'file-editor-changed' => 'Changed file editor from :old to :new',
+            'avatar-changed' => 'Changed avatar style from :old to :new',
         ],
         'api-key' => [
             'create' => 'Yeni API anahtarı :identifier oluşturuldu',
