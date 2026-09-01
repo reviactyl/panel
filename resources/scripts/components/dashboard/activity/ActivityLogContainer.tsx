98f1ceeb0a57c7dadaf4eb71d12deb23426a78eb
jasonzli-DEV <jasonzxl@icloud.com>
2026-08-31 22:22:18 -0400

    fix activity log filter pagination

--- resources/scripts/components/dashboard/activity/ActivityLogContainer.tsx
+++ resources/scripts/components/dashboard/activity/ActivityLogContainer.tsx
@@ -25,7 +25,7 @@ export default () => {
     });
 
     useEffect(() => {
-        setFilters((value) => ({ ...value, filters: { ip: hash.ip, event: hash.event } }));
+        setFilters((value) => ({ ...value, page: 1, filters: { ip: hash.ip, event: hash.event } }));
     }, [hash]);
 
     useEffect(() => {
@@ -40,7 +40,7 @@ export default () => {
                     <Link
                         to={'#'}
                         className={classNames(btnStyles.button, btnStyles.text, 'w-full sm:w-auto')}
-                        onClick={() => setFilters((value) => ({ ...value, filters: {} }))}
+                        onClick={() => setFilters((value) => ({ ...value, page: 1, filters: {} }))}
                     >
                         {t('activity.clear-filters')} <XCircleIcon className={'w-4 h-4 ml-2'} />
                     </Link>
