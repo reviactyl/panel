b4b88f9d4ab1ed9cf69556f7e1e5c2520c28f1d8
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-09-16 03:48:53 +0530

    New translations navigation.php (Russian)
    [ci skip]

--- resources/lang/ru/admin/navigation.php
+++ resources/lang/ru/admin/navigation.php
@@ -8,8 +8,12 @@ return [
         'settings' => 'Настройки',
         'api' => 'API приложения',
         'activity_log' => 'Журнал активности',
-        'user_activity_metrics' => 'Метрики активности пользователей',
+        'user_activity_metrics' => 'Active User Locations',
+        'user_activity_description' => 'Location of active users in the past 30 days.',
+        'active_users' => 'Active users',
         'no_data' => 'Нет доступных данных.',
+        'other' => 'Other',
+        'unknown' => 'Unknown',
     ],
     'management' => [
         'title' => 'Управление',
