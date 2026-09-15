e32d71ee7a4f45fe0b7da37123172df95e37a6da
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-09-16 03:48:55 +0530

    New translations navigation.php (Swedish)
    [ci skip]

--- resources/lang/sv/admin/navigation.php
+++ resources/lang/sv/admin/navigation.php
@@ -8,8 +8,12 @@ return [
         'settings' => 'Inställningar',
         'api' => 'Applikations-API',
         'activity_log' => 'Aktivitetslogg',
-        'user_activity_metrics' => 'Användaraktivitetsstatistik',
+        'user_activity_metrics' => 'Active User Locations',
+        'user_activity_description' => 'Location of active users in the past 30 days.',
+        'active_users' => 'Active users',
         'no_data' => 'Inga data tillgängliga.',
+        'other' => 'Other',
+        'unknown' => 'Unknown',
     ],
     'management' => [
         'title' => 'Hantering',
