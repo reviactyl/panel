7847e7b522098475050fa8a08e79709c2de515e4
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-09-17 18:54:22 +0530

    New translations node.php (Hindi)
    [ci skip]

--- resources/lang/hi/admin/node.php
+++ resources/lang/hi/admin/node.php
@@ -126,8 +126,8 @@ return [
     'table' => [
         'health' => 'स्वास्थ्य',
         'health_http_status' => 'HTTP :स्थिति',
-        'health_agent_outdated' => 'Agent outdated. v:version is available. Click here to update Agent.',
         'health_check_console' => 'ब्राउज़र कंसोल की जाँच करें',
+        'health_agent_outdated' => 'Agent outdated. v:version is available. Click here to update Agent.',
         'id' => 'आईडी',
         'uuid' => 'यूयूआईडी',
         'name' => 'नाम',
