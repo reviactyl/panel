d4a8b17c7e12d065125a7825275b14b0e6223a72
Brijesh Bangera <103484184+BijjuXD@users.noreply.github.com>
2026-09-17 18:53:54 +0530

    New translations node.php (Swedish)
    [ci skip]

--- resources/lang/sv/admin/node.php
+++ resources/lang/sv/admin/node.php
@@ -126,8 +126,8 @@ return [
     'table' => [
         'health' => 'Hälsa',
         'health_http_status' => 'HTTP :status',
-        'health_agent_outdated' => 'Agent outdated. v:version is available. Click here to update Agent.',
         'health_check_console' => 'kolla webbläsarkonsolen',
+        'health_agent_outdated' => 'Agent outdated. v:version is available. Click here to update Agent.',
         'id' => 'ID',
         'uuid' => 'UUID',
         'name' => 'Namn',
